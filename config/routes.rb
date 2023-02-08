41b72d586f1c456bbfe82fdf85d232a5b6e4e0a0
m.ranocchia <m.ranocchia@parkit.it>
2023-02-08 11:24:33 +0100

    creata about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   #impostare pages/home come pagina principale collegandomi
   root 'pages#home'
+
+  get 'about' => 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
